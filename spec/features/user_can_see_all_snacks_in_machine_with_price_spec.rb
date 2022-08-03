12d7f73f5298bac21113941dfad5df42a7a55388
Nikhil Shahi <shniks@gmail.com>
2022-08-03 16:34:55 -0700

    Refactored user story 1 feature test.

--- spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
+++ spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'When a user visits the vending machine show page' do
   describe 'they see a list of all snacks associated with that machine' do
-    it 'they also see the price of each snack' do
+    scenario 'they also see the price of each snack' do #user story 1
 
       owner = Owner.create!(name: 'Big Al')
       machine = owner.machines.create!(location: 'Hollywood')
-      snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: '$3.50')
-      snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: '$1.50')
-      snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: '$2.50')
+      snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: 3.50)
+      snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: 1.50)
+      snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: 2.50)
 
       visit "/machines/#{machine.id}"
 
