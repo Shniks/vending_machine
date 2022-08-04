93c4f79738e0ffbed8ad0d61dc8b16bb6243a8fb
Nikhil Shahi <shniks@gmail.com>
2022-08-03 20:01:33 -0700

    Created feature test for user story 2 to find the average price of snacks per machine.

--- spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
+++ spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe 'When a user visits the vending machine show page' do
     expect(page).to have_content('* Pop Rocks: $1.50')
     expect(page).to have_content('* Flaming Hot Cheetos: $2.50')
   end
+
+  scenario 'they also see an average price of all snacks in the machine' do #user story 2
+
+    owner = Owner.create!(name: 'Big Al')
+    machine = owner.machines.create!(location: 'Hollywood')
+    snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: 3.50)
+    snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: 1.50)
+    snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: 2.50)
+
+    visit "/machines/#{machine.id}"
+
+    expect(page).to have_content('Average Price: $2.50')
+  end
 end
