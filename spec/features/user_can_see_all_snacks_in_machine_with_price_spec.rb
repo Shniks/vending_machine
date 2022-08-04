44926c480379c9e470e16dde2dd85c828c08c894
Nikhil Shahi <shniks@gmail.com>
2022-08-03 18:22:10 -0700

    Refactored user story 1 feature test. Added relationships assertions to snack and machine model tests.

--- spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
+++ spec/features/user_can_see_all_snacks_in_machine_with_price_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'When a user visits the vending machine show page' do
-  describe 'they see a list of all snacks associated with that machine' do
-    scenario 'they also see the price of each snack' do #user story 1
+  scenario 'they see a list of all snacks associated with that machine' do #user story 1
 
-      owner = Owner.create!(name: 'Big Al')
-      machine = owner.machines.create!(location: 'Hollywood')
-      snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: 3.50)
-      snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: 1.50)
-      snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: 2.50)
+    owner = Owner.create!(name: 'Big Al')
+    machine = owner.machines.create!(location: 'Hollywood')
+    snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: 3.50)
+    snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: 1.50)
+    snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: 2.50)
 
-      visit "/machines/#{machine.id}"
+    visit "/machines/#{machine.id}"
 
-      expect(page).to have_content('* White Castle Burger: $3.50')
-      expect(page).to have_content('* Pop Rocks: $1.50')
-      expect(page).to have_content('* Flaming Hot Cheetos: $2.50')
-    end
+    expect(page).to have_content('* White Castle Burger: $3.50')
+    expect(page).to have_content('* Pop Rocks: $1.50')
+    expect(page).to have_content('* Flaming Hot Cheetos: $2.50')
   end
 end
