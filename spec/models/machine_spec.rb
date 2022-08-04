0e6026447465591d37706514dc8dbe3a35d16182
Nikhil Shahi <shniks@gmail.com>
2022-08-03 20:14:17 -0700

    Created model test to find average price for snacks in a machine.

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+
+  it 'it computes the average price of snacks' do
+    owner = Owner.create!(name: 'Big Al')
+    machine = owner.machines.create!(location: 'Hollywood')
+    snack_1 = machine.snacks.create!(name: 'White Castle Burger', price: 3.50)
+    snack_2 = machine.snacks.create!(name: 'Pop Rocks', price: 1.50)
+    snack_3 = machine.snacks.create!(name: 'Flaming Hot Cheetos', price: 2.50)
+    snack_4 = machine.snacks.create!(name: 'Doritos', price: 8.00)
+    snack_5 = machine.snacks.create!(name: 'Cheese', price: 6.25)
+
+    expect(machine.average_price).to eq(4.35)
+  end
 end
