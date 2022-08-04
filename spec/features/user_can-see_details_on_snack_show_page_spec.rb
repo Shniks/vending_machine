b4a2c596a58371de3dc7b3e70eb8f8bd0c579b39
Nikhil Shahi <shniks@gmail.com>
2022-08-04 11:55:35 -0700

    Refactored feature test for snack show page to show the price and name of the snack. Added before do statement.

--- spec/features/user_can-see_details_on_snack_show_page_spec.rb
+++ spec/features/user_can-see_details_on_snack_show_page_spec.rb
@@ -2,32 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   describe 'when I visit the a snack show page' do
-    it 'I see name and price of that snack' do
 
+    before do
       owner = Owner.create!(name: 'Big Al')
       machine_1 = owner.machines.create!(location: 'Hollywood')
       machine_2 = owner.machines.create!(location: 'Sunset')
       machine_3 = owner.machines.create!(location: 'La Canada')
       machine_4 = owner.machines.create!(location: 'Arcadia')
       machine_5 = owner.machines.create!(location: 'Downey')
-      snack_1 = Snack.create!(name: 'White Castle Burger', price: 3.50)
-      snack_2 = Snack.create!(name: 'Pop Rocks', price: 1.50)
-      snack_3 = Snack.create!(name: 'Lays Chips', price: 2.75)
+      @snack_1 = Snack.create!(name: 'White Castle Burger', price: 3.50)
+      @snack_2 = Snack.create!(name: 'Pop Rocks', price: 1.50)
+      @snack_3 = Snack.create!(name: 'Lays Chips', price: 2.75)
 
-      MachineSnack.create!(snack: snack_1, machine: machine_1)
-      MachineSnack.create!(snack: snack_1, machine: machine_3)
-      MachineSnack.create!(snack: snack_1, machine: machine_5)
-      MachineSnack.create!(snack: snack_2, machine: machine_1)
-      MachineSnack.create!(snack: snack_2, machine: machine_4)
-      MachineSnack.create!(snack: snack_3, machine: machine_2)
-      MachineSnack.create!(snack: snack_3, machine: machine_3)
-      MachineSnack.create!(snack: snack_3, machine: machine_4)
-      MachineSnack.create!(snack: snack_3, machine: machine_5)
-      
-      visit "/snacks/#{snack_1.id}"
+      MachineSnack.create!(snack: @snack_1, machine: machine_1)
+      MachineSnack.create!(snack: @snack_1, machine: machine_3)
+      MachineSnack.create!(snack: @snack_1, machine: machine_5)
+      MachineSnack.create!(snack: @snack_2, machine: machine_1)
+      MachineSnack.create!(snack: @snack_2, machine: machine_2)
+      MachineSnack.create!(snack: @snack_2, machine: machine_4)
+      MachineSnack.create!(snack: @snack_3, machine: machine_2)
+      MachineSnack.create!(snack: @snack_3, machine: machine_3)
+      MachineSnack.create!(snack: @snack_3, machine: machine_4)
+      MachineSnack.create!(snack: @snack_3, machine: machine_5)
+    end
+
+    it 'I see name and price of that snack' do
+
+      visit "/snacks/#{@snack_1.id}"
 
       expect(page).to have_content('White Castle Burger')
       expect(page).to have_content('Price: $3.50')
     end
+
   end
 end
